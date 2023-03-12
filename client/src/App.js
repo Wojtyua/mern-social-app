b69311de297924b11d985c8c29e31c52b7f1518b
Wojtyua <72139361+Wojtyua@users.noreply.github.com>
2023-03-12 19:41:36 +0100

    creating material theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,30 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "./scenes/homePage";
+import LoginPage from "./scenes/loginPage";
+import ProfilePage from "./scenes/profilPage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme.js";
+
 const App = () => {
+  // set the theme based on the mode
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings[mode]), [mode]);
+
   return (
     <div className="app">
-      <h1>siema</h1>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 };
