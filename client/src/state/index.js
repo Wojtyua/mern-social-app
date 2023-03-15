af2a4f0155d1f2b141e0b2a29485bc8db47a086b
Wojtyua <72139361+Wojtyua@users.noreply.github.com>
2023-03-16 00:02:14 +0100

    redux slice started

---
+++ client/src/state/index.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+  token: null,
+  posts: [],
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setLogin: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+  },
+});
